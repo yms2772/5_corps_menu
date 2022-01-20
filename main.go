cf269a4870317fae89ec42eac08e125634943587
Mokky <a53312773@gmail.com>
2022-01-20 20:02:15 +0900

    20220120 업데이트
    1. 휴가제외 전역 프로그레스바 추가
    2. 기본폰트 삭제
    3. 프로그레스바 1초 refresh 적용

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+	"time"
 )
 
 func main() {
@@ -11,6 +16,53 @@ func main() {
 
 	mainWindow := mainApp.NewWindow("5군단 식단")
 
-	mainWindow.SetContent(container.NewBorder(progressBar(mainApp, mainWindow), nil, nil, nil, container.NewAppTabs(menuItem(mainWindow), settingsItem(mainApp, mainWindow))))
+	enterArmyDate := mainApp.Preferences().StringWithFallback("enter_army_date", "none")
+	dischargeArmyDate := mainApp.Preferences().StringWithFallback("discharge_army_date", "none")
+	vacationDay := mainApp.Preferences().IntWithFallback("vacation", 0)
+
+	if enterArmyDate == "none" || dischargeArmyDate == "none" {
+		dialog.ShowError(fmt.Errorf("설정에서 입대일과 전역일을 설정하세요"), mainWindow)
+	}
+
+	enterArmyDateTime, _ := time.Parse("20060102", enterArmyDate)
+	dischargeArmyDateTime, _ := time.Parse("20060102", dischargeArmyDate)
+
+	totalArmyDateNanoSeconds := dischargeArmyDateTime.Sub(enterArmyDateTime).Nanoseconds()
+	totalWOVArmyDateNanoSeconds := dischargeArmyDateTime.AddDate(0, 0, -vacationDay).Sub(enterArmyDateTime).Nanoseconds()
+
+	diff := float64(time.Now().Sub(enterArmyDateTime).Nanoseconds()) / float64(totalArmyDateNanoSeconds)
+	diffWOV := float64(time.Now().Sub(enterArmyDateTime).Nanoseconds()) / float64(totalWOVArmyDateNanoSeconds)
+
+	diffData := binding.BindFloat(&diff)
+	diffWOVData := binding.BindFloat(&diffWOV)
+
+	go func() {
+		ticker := time.NewTicker(time.Second)
+
+		for range ticker.C {
+			if err := diffData.Set(float64(time.Now().Sub(enterArmyDateTime).Nanoseconds()) / float64(totalArmyDateNanoSeconds)); err != nil {
+				fmt.Println(err)
+			}
+
+			if err := diffWOVData.Set(float64(time.Now().Sub(enterArmyDateTime).Nanoseconds()) / float64(totalWOVArmyDateNanoSeconds)); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}()
+
+	totalProgress := widget.NewProgressBarWithData(diffData)
+	totalProgress.TextFormatter = func() string {
+		return fmt.Sprintf("%f%% (D-%d)", totalProgress.Value*100, int(dischargeArmyDateTime.Sub(enterArmyDateTime).Hours()/24)-int(time.Now().Sub(enterArmyDateTime).Hours()/24))
+	}
+
+	totalWOV := widget.NewProgressBarWithData(diffWOVData)
+	totalWOV.TextFormatter = func() string {
+		return fmt.Sprintf("%f%% (D-%d)", totalWOV.Value*100, int(dischargeArmyDateTime.AddDate(0, 0, -vacationDay).Sub(enterArmyDateTime).Hours()/24)-int(time.Now().Sub(enterArmyDateTime).Hours()/24))
+	}
+
+	remainProgress := container.NewVBox(totalProgress, totalWOV)
+	tabItem := container.NewAppTabs(menuItem(mainWindow), settingsItem(mainApp, mainWindow))
+
+	mainWindow.SetContent(container.NewBorder(remainProgress, nil, nil, nil, tabItem))
 	mainWindow.ShowAndRun()
 }
--- settings.go
+++ settings.go
@@ -26,6 +26,33 @@ func settingsItem(a fyne.App, w fyne.Window) *container.TabItem {
 		dischargeArmyDateEntry.SetText(data)
 	}
 
+	remainedVacationWarn := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{})
+	remainedVacationWarn.Hide()
+
+	remainedVacation := widget.NewEntry()
+	remainedVacation.SetText(fmt.Sprintf("%d", a.Preferences().IntWithFallback("vacation", 0)))
+	remainedVacation.Validator = func(s string) error {
+		if len(s) == 0 {
+			return nil
+		}
+
+		if _, err := strconv.Atoi(s); err != nil {
+			return fmt.Errorf("숫자만 입력할 수 있습니다")
+		}
+
+		return nil
+	}
+	remainedVacation.SetOnValidationChanged(func(err error) {
+		if err != nil {
+			remainedVacationWarn.SetText(fmt.Sprintf("* %s", err.Error()))
+			remainedVacationWarn.Show()
+		} else {
+			remainedVacationWarn.Hide()
+		}
+	})
+
+	remainedVacationBox := container.NewHBox(remainedVacation, widget.NewLabel("일"), remainedVacationWarn)
+
 	armySelect := widget.NewSelect([]string{"5군단"}, func(s string) {
 
 	})
@@ -58,14 +85,31 @@ func settingsItem(a fyne.App, w fyne.Window) *container.TabItem {
 				return
 			}
 
+			if data, err := strconv.Atoi(remainedVacation.Text); err != nil {
+				dialog.ShowError(fmt.Errorf("남은 휴가일은 숫자만 입력할 수 있습니다"), w)
+
+				return
+			} else {
+				a.Preferences().SetInt("vacation", data)
+			}
+
 			a.Preferences().SetString("enter_army_date", enterArmyDateEntry.Text)
 			a.Preferences().SetString("discharge_army_date", dischargeArmyDateEntry.Text)
 
-			dialog.ShowInformation("설정", "저장완료", w)
+			reloadApp := dialog.NewConfirm("설정", "앱을 재시작해야 설정이 반영됩니다", func(b bool) {
+				if b {
+					a.Quit()
+				}
+			}, w)
+			reloadApp.SetConfirmText("재시작")
+			reloadApp.SetDismissText("취소")
+			reloadApp.Show()
 		},
 	}
+
 	form.Append("입대일", enterArmyDateEntry)
 	form.Append("전역일", dischargeArmyDateEntry)
+	form.Append("남은 휴가일", remainedVacationBox)
 	form.Append("부대", armySelect)
 
 	return container.NewTabItemWithIcon("설정", theme.SettingsIcon(),
